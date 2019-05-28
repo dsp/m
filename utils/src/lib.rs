075f41f90660bf9bd8ebbfcc96b67a7727e2eb8e
David Soria Parra <dsp@experimentalworks.net>
2019-05-27 19:38:47 -0700

    Add a simple utility module

---
+++ utils/src/lib.rs
@@ -0,0 +1,55 @@
+#[derive(Debug, PartialEq, Eq)]
+pub enum WatchStatus {
+    NeedsUpdate,
+    Nothing,
+}
+
+//pub trait Watchable {
+//    fn watch() -> Watch;
+//}
+
+pub struct Watch<'a, F, T>
+where
+    F: Fn() -> WatchStatus
+{
+    watchfn: F,
+    reference: &'a T,
+}
+
+impl<'a, F, T> Watch<'a, F, T>
+where
+    F: Fn() -> WatchStatus
+{
+    pub fn new(watchfn: F, reference: &'a T) -> Self {
+        Self {
+            watchfn,
+            reference,
+        }
+    }
+
+    pub fn status(&self) -> WatchStatus {
+        (self.watchfn)()
+    }
+
+    pub fn into_raw(&self) -> &'a T {
+        self.reference
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    #[test]
+    fn simple_status() {
+        let a = 2;
+        let watch = Watch::new(|| { WatchStatus::NeedsUpdate }, &a);
+        assert_eq!(watch.status(), WatchStatus::NeedsUpdate);
+    }
+
+    #[test]
+    fn simple_ref() {
+        let a = 2;
+        let watch = Watch::new(|| { WatchStatus::NeedsUpdate }, &a);
+        assert_eq!(watch.into_raw(), &a);
+    }
+}
